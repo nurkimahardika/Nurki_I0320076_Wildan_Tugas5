455037f0aa9b02f01b1439541c1dc4cf7a12b735
Nurki Putra Mahardika <nurput28@gmail.com>
2021-03-29 18:59:08 +0700

    Exercise 5.1

---
+++ exercise 5.1.py
@@ -0,0 +1,6 @@
+#penggunaan if untuk satu kasus
+#inoutkan bilangan
+bilangan = int(input('Masukkan bilangan bulat: '))
+#memeriksa bilangan
+if bilangan%2 == 0:
+    print('%d adalah bilangan genap' % bilangan)
