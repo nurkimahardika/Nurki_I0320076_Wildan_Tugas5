b6d0d8dd083cc72e1f4477e9adcf86dde2e3c7f2
Nurki Putra Mahardika <nurput28@gmail.com>
2021-03-29 20:05:28 +0700

    Soal 1

---
+++ soal 1.py
@@ -0,0 +1,6 @@
+nama = str(input('Siapa nama anda?'))
+jenis_kelamin = str(input('Pilih jenis kelamin! (L/P)'))
+if jenis_kelamin == 'L':
+    print('Selamat datang, Bapak', nama,'!')
+else :
+    print('Selamat datang, Ibu', nama,'!')
